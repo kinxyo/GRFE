daad04bb4d6b04a3d9de466c75b76430167271d1
kinxyos <90744941+kinxyo@users.noreply.github.com>
2023-09-26 17:10:42 +0530

    ssr false exp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  ssr: false,
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
   },
@@ -8,16 +8,13 @@ export default defineNuxtConfig({
   googleFonts: {
     families: {
       "Space+Grotesk": true,
-      "Open+Sans": true,
       "Pathway+Extreme": true,
       "Josefin+Sans": true,
-      "Sedgwick+Ave+Display": true,
       "Titillium+Web": true,
       Raleway: true,
       Comme: true,
       Inter: true,
       Poppins: true,
-      Inconsolata: true,
       Rubik: true,
       Ysabeau: true,
       Sigmar: true,
